e5870b2684bd4b668bda0ec5cc1a3846eb9e5cec
mantvmass <kliop2317@gmail.com>
2024-05-18 20:31:25 +0700

    📦 build: New build script

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,1 +1,11 @@
-python setup.py sdist
+@echo off
+
+@REM clear old build folder
+rmdir /s /q dist
+rmdir /s /q build
+rmdir /s /q ldplayer_tools.egg-info
+
+@REM building...
+python setup.py sdist bdist_wheel
+
+@REM pause
--- scripts/publish.cmd
+++ scripts/publish.cmd
@@ -1,1 +1,2 @@
+@echo off
 twine upload dist/*
